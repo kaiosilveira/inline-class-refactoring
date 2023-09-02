ffc8c8742032d143466395f6004e923d23fc21f5
Kaio Silveira <silveira.kaio@icloud.com>
2023-09-02 14:49:45 +0100

    inline `trackingInformation.display` into `Shipment.trackingInfo`

--- src/shipment/index.js
+++ src/shipment/index.js
@@ -1,6 +1,6 @@
 export class Shipment {
   get trackingInfo() {
-    return this._trackingInformation.display;
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
 
   get trackingInformation() {
